1c4651c11d2fd7c9ce10e7120e52a4ce8f93b342
Amanda <amandaben21@aol.com>
2021-04-29 18:35:00 -0400

    Done

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-function addingEventListener() {
+const input = document.getElementById('input');
+
+function addingEventListener() {    //binds an event listener in addingEventListener()
+    input.addEventListener('click', function(event) {
+    alert('I was clicked!');
+  }); 
 }
